70336f37dfe9843815dd47a5f0e3db0e7da96cf4
wildpow <element1028@gmail.com>
2021-03-17 21:00:03 -0700

    chore: setting up a gatsby v3 version of esc

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    "gatsby-plugin-webpack-bundle-analyser-v2",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === "production") {
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+}
