0672bd393f54c0723524c29752e59e90b05af5f8
wildpow <element1028@gmail.com>
2021-03-31 18:45:36 -0700

    testing new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
+        warmGray: colors.warmGray,
+        lightBlue: colors.lightBlue,
+        rose: colors.rose,
+      },
+    },
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
